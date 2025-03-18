b072df7cf1a39808546f9e8256aa046fcaac6c81
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-19 01:35:20 +0300

    add get_async_session

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
-from typing import Annotated
+from typing import Annotated, AsyncGenerator
 
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, declared_attr, mapped_column
 from sqlalchemy.types import String, Integer
 
@@ -11,22 +11,17 @@ DATABASE_URL = get_db_url()
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
-
+    
 # настройка аннотаций
 int_pk = Annotated[int, mapped_column(Integer, primary_key=True)]
 str_uniq = Annotated[str, mapped_column(String, unique=True, nullable=False)]
 
-
-class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = True
-
-    @declared_attr.directive
-    def __tablename__(cls) -> str:
-        return f"{cls.__name__.lower()}s"
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
