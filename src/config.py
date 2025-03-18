c91e1b2efe96c4b92773eca086da3024c46ab775
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-19 01:34:28 +0300

    add hash settings

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os
-from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from passlib.context import CryptContext
 
 class Settings(BaseSettings):
     DB_HOST: str
@@ -8,13 +9,15 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_USER: str
     DB_PASS: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
     )
 
-
 settings = Settings()
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_db_url():
     return (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASS}@"
