cb4d25c8d065a4cb16ac813eb79e69efd77e9f22
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-19 14:59:53 +0300

    add token endpoint

--- src/users/auth.py
+++ src/users/auth.py
@@ -1,20 +1,24 @@
+from typing import Annotated
+
 from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, insert, text
+from datetime import timedelta
 
-from src.users.schemas import UserData, RegUser, Message
+from src.users.schemas import RegUser, Message, Token
 from src.database import get_async_session
 from src.users.models import User
-from src.users.service import get_password_hash
+from src.users.service import get_password_hash, get_user, authenticate_user, create_access_token
+from src.config import settings
 
 router = APIRouter(prefix='/auth', tags=['Auth'])
 
 @router.post("/register/", status_code=status.HTTP_201_CREATED)
 async def register_user(user_data: RegUser, session: AsyncSession = Depends(get_async_session)) -> Message:
     user_dict = user_data.model_dump()
-    query = select(User).where(User.username == user_dict['username'])
-    user = await session.execute(query)
-    if user.first():
+    user = await get_user(user_dict['username'], session)
+    if user:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail='This user already exists.'
@@ -29,3 +33,21 @@ async def register_user(user_data: RegUser, session: AsyncSession = Depends(get_
         return Message(text='Registration completed!')
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: AsyncSession = Depends(get_async_session),
+) -> Token:
+    user = await authenticate_user(form_data.username, form_data.password, session)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
