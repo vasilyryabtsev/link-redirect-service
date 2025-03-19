797b9b1a41215e2713d87de4afae4bb8e13255ba
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-20 00:17:35 +0300

    fix bug

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import uvicorn
 from fastapi import FastAPI
+from src.users.auth import router as auth_router
 
-from src.users.auth import router as auth_rounter
 
 app = FastAPI()
-app.include_router(auth_rounter)
+
+app.include_router(auth_router)
