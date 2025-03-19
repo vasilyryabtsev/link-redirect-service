6b7494381af99edcf6cf0dcc458257b9da4d5866
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-19 14:59:24 +0300

    update

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,13 +1,24 @@
 from pydantic import BaseModel
 
+
 class RegUser(BaseModel):
     username: str
     password: str
     
+# User
 class UserData(BaseModel):
     username: str | None = None
-    hashed_password: str | None = None
     disabled: bool | None = None
     
 class Message(BaseModel):
     text: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class UserInDB(UserData):
+    hashed_password: str
--- src/users/service.py
+++ src/users/service.py
@@ -1,5 +1,37 @@
-from src.config import pwd_context
+import jwt
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from datetime import datetime, timedelta, timezone
+
+from src.config import pwd_context, settings
+from src.users.models import User
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+async def get_user(username: str, session: AsyncSession):
+    query = select(User).where(User.username == username)
+    result = await session.execute(query)
+    return result.scalar_one_or_none()
+    
+async def authenticate_user(username: str, password: str, session: AsyncSession):
+    user = await get_user(username, session)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
