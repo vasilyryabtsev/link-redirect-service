2fbfd81a53c5c2b8972bb19845489cc3b6c97b70
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-20 19:36:13 +0300

    add read_users_me

---
+++ src/users/router.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends
+
+from src.users.schemas import UserData
+from src.users.models import User
+from src.users.service import get_current_active_user
+
+router = APIRouter(prefix='/user', tags=['User'])
+
+@router.get("/me/", response_model=UserData)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
