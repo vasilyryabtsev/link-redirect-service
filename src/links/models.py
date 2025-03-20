bb55809ea4011b2a21a6da3b0dd0f42886ee78f4
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-20 19:33:16 +0300

    change columns

--- src/links/models.py
+++ src/links/models.py
@@ -10,20 +10,20 @@ from src.users.models import User
 
 class Link(Base):
     id: Mapped[int_pk]
-    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped[int] = mapped_column(ForeignKey("users.username"), nullable=True)
     link: Mapped[str_uniq]
-    short_link: Mapped[str] = mapped_column(String, nullable=False)
+    code: Mapped[str] = mapped_column(String, nullable=False)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=datetime.now)
     usage_count: Mapped[int] = mapped_column(server_default=text('0'))
     
-    user: Mapped["User"] = relationship("User", back_populates="shortlinks")
+    user: Mapped["User"] = relationship("User", back_populates="links")
 
     def __repr__(self):
         return (f"{self.__class__.__name__}(id={self.id}, "
-                f"owner_id={self.owner_id!r}, "
+                f"owner={self.owner!r}, "
                 f"link={self.link!r}, "
-                f"short_link={self.short_link!r}, "
+                f"code={self.code!r}, "
                 f"created_at={self.created_at!r}, "
                 f"updated_at={self.updated_at!r}, "
                 f"usage_count={self.usage_count!r})")
