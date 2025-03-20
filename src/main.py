673601d7aeeddb8263383f308c312f181fd7ee91
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-20 19:32:48 +0300

    add new router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from src.users.auth import router as auth_router
+from src.users.router import router as user_router
+from src.links.router import router as link_router
 
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(user_router)
+app.include_router(link_router)
