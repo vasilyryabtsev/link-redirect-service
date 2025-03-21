99e19e62396695e2d8925074548294a3761457a8
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-21 23:07:53 +0300

    fix types, separate funcs

--- src/links/router.py
+++ src/links/router.py
@@ -1,31 +1,34 @@
-from typing import Annotated, Optional
+from typing import Optional
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import insert
 
-from src.links.service import get_code
+from src.links.service import get_code, get_link, insert_link, update_link
 from src.links.schemas import NewLink
 from src.users.schemas import UserData
 from src.users.service import get_current_active_user_soft
 from src.database import get_async_session
-from src.links.models import Link
+from src.schemas import Message
+
 
 router = APIRouter(prefix='/links', tags=['Link'])
 
 @router.post("/shorten/", status_code=status.HTTP_201_CREATED)
 async def read_users_me(
-    link: NewLink,
+    link_data: NewLink,
     session: AsyncSession = Depends(get_async_session),
     current_user: Optional[UserData] = Depends(get_current_active_user_soft)
-):
-    link_data = link.model_dump()
+) -> Message:
+    link_dict = link_data.model_dump()
+    link_old = await get_link(link_dict['link'], session)
+    if link_old:
+        raise HTTPException(
+            status_code=status.HTTP_208_ALREADY_REPORTED,
+            detail=f'This link already has a short version: {link_old.code}'
+        )
     if current_user:
-        link_data['owner'] = current_user.model_dump()['username']
-    link_data['code'] =  get_code(link_data['link'])
-    statement = insert(Link).values(link_data)
-    try:
-        await session.execute(statement)
-        await session.commit()
-        return {"text": f"Your short link: {link_data['code']}"}
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))    
+        link_dict['owner'] = current_user.model_dump()['username']
+    await insert_link(session, **link_dict)
+    link_new = await get_link(link_dict['link'], session)
+    code = get_code(link_new.id)
+    await update_link(session, link_new.id, code)
+    return Message(message=f"Your short link: {code}")
