061d42e0c5bb9a0a575a7eb7cf31e1e720c8cdf9
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-21 23:08:33 +0300

    change get_code, add new funcs

--- src/links/service.py
+++ src/links/service.py
@@ -1,8 +1,31 @@
-from typing import Annotated
-from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, insert, update
+from fastapi import status, HTTPException
 
-from src.users.service import get_current_user
-from src.users.schemas import UserData
-    
-def get_code(link):
-    return '123'
+from src.links.models import Link
+from src.config import settings, sqids
+
+
+def get_code(link_id: int) -> str:
+    return sqids.encode(list(range(link_id, link_id + settings.LINK_ENCODING_SIZE)))
+
+async def get_link(link: str, session: AsyncSession) -> (Link | None):
+    query = select(Link).where(Link.link == link)
+    result = await session.execute(query)
+    return result.scalar_one_or_none()
+
+async def insert_link(session: AsyncSession, link: str, owner: str | None = None) -> None:
+    statement = insert(Link).values(link=link, owner=owner)
+    try:
+        await session.execute(statement)
+        await session.commit()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+async def update_link(session: AsyncSession, id: int, code: str) -> None:
+    statement = update(Link).where(Link.id == id).values(code=code)
+    try:
+        await session.execute(statement)
+        await session.commit()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
