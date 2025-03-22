b5481ae64ead28330600805ec651ccb0d088b6e3
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-23 01:09:04 +0300

    add new schemas

--- src/links/schemas.py
+++ src/links/schemas.py
@@ -1,9 +1,22 @@
+from typing import Optional
 from pydantic import BaseModel, HttpUrl, field_serializer
+from datetime import datetime
 
-    
-class NewLink(BaseModel):
+class Url(BaseModel):
     link: HttpUrl
     
     @field_serializer('link')
     def serealize_link(self, link: HttpUrl):
         return str(link)
+
+class CustomUrl(Url):
+    expires_at: datetime
+    custom_alias: Optional[str] = None
+    
+    
+class LinkData(BaseModel):
+    link: str
+    code: str
+    created_at: datetime
+    usage_count: int
+    updated_at: datetime
