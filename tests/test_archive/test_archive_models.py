93d94e358f08e537cdada7b8ae3550f3d4ab40cc
vasilyryabtsev <vasilyryabtsev@gmail.com>
2025-03-31 23:05:18 +0300

    fix

--- tests/test_archive/test_archive_models.py
+++ tests/test_archive/test_archive_models.py
@@ -1,23 +1,29 @@
+import pytest
+
 from datetime import datetime
 
-from archive.models import ArchivedLink
+from src.archive.models import ArchivedLink
 
-def test_archivedlink_repr():
+
+@pytest.fixture()
+def valid_archivedlink():
     link = ArchivedLink(
-        id=1,
         owner="user",
         link="http://example.com/",
-        code="Zdjflks32",
-        created_at='2025-03-29 17:52:46.794823',
-        deleted_at='2025-03-29 17:52:46.794823',
+        code="example_code",
+        created_at=datetime.now(),
+        deleted_at=datetime.now(),
         usage_count=0
     )
-    assert link.__repr__() == (
-        f"ArchivedLink(id=1, "
-        f"owner='user', "
-        f"link='http://example.com/', "
-        f"code='Zdjflks32', "
-        f"created_at='2025-03-29 17:52:46.794823', "
-        f"deleted_at='2025-03-29 17:52:46.794823', "
-        f"usage_count=0)"
+    return link
+
+def test_archivedlink_repr(valid_archivedlink):
+    assert valid_archivedlink.__repr__() == (
+        f"ArchivedLink(id={valid_archivedlink.id!r}, "
+        f"owner={valid_archivedlink.owner!r}, "
+        f"link={valid_archivedlink.link!r}, "
+        f"code={valid_archivedlink.code!r}, "
+        f"created_at={valid_archivedlink.created_at!r}, "
+        f"deleted_at={valid_archivedlink.deleted_at!r}, "
+        f"usage_count={valid_archivedlink.usage_count!r})"
     )
--- tests/test_links/test_links_models.py
+++ tests/test_links/test_links_models.py
@@ -1,25 +1,31 @@
+import pytest
+
 from datetime import datetime
 
-from links.models import Link
+from src.links.models import Link
 
-def test_link_repr():
+
+@pytest.fixture()
+def valid_link():
     link = Link(
-        id=1,
-        owner="user",
+        owner=None,
         link="http://example.com/",
-        code="Zdjflks32",
-        created_at='2025-03-29 17:52:46.794823',
-        updated_at='2025-03-29 17:52:46.794823',
+        code="example_code",
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
         usage_count=0,
-        expires_at='2025-03-29 17:52:46.794823'
+        expires_at=datetime.now()
     )
-    assert link.__repr__() == (
-        f"Link(id=1, "
-        f"owner='user', "
-        f"link='http://example.com/', "
-        f"code='Zdjflks32', "
-        f"created_at='2025-03-29 17:52:46.794823', "
-        f"updated_at='2025-03-29 17:52:46.794823', "
-        f"usage_count=0, "
-        f"expires_at='2025-03-29 17:52:46.794823')"
+    return link
+
+def test_link_repr(valid_link):
+    assert valid_link.__repr__() == (
+        f"Link(id={valid_link.id!r}, "
+        f"owner={valid_link.owner!r}, "
+        f"link={valid_link.link!r}, "
+        f"code={valid_link.code!r}, "
+        f"created_at={valid_link.created_at!r}, "
+        f"updated_at={valid_link.updated_at!r}, "
+        f"usage_count={valid_link.usage_count!r}, "
+        f"expires_at={valid_link.expires_at!r})"
     )
